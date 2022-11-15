aa6780b0e95bf97f806d62346e677344b70aa610
samuelcpr <samuelcpr464@gmail.com>
2022-11-15 17:07:23 -0300

    first commit

---
+++ Pasta_jogo/jogodaadivinhacao.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+  std::cout << "*************************************" << std::endl;
+  std::cout << "*Bem-vindos ao jogo da adivinhação! *" << std::endl;
+  std::cout << "*************************************" << std::endl;
+}
